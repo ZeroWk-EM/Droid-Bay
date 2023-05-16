2a27abb74be39d4aee62ce75f49b6752170b1d11
Emanuele Moncada <em@zerowk.it>
2023-05-16 19:49:41 +0200

    fix(filer): update filtering

--- client/src/components/Filter/Filter.tsx
+++ client/src/components/Filter/Filter.tsx
@@ -1,51 +1,51 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-const Filter = () => {
+type SelectType = {
+  setStatus: Function;
+  setCategory: Function;
+};
+
+const Filter = (props: SelectType) => {
+  const changeSelectStatus = (event: any) => setStatus(event.target.value);
+  const changeSelectCategory = (event: any) => setCategory(event.target.value);
+  const { setStatus, setCategory } = props;
+
   return (
     <div>
       <h4>Add Filter</h4>
       <form>
         <label htmlFor="typeList" className="form-label">
-          Select type
+          Select category
         </label>
         <select
           className="form-select"
           aria-label="Default select example"
           id="typeList"
+          onChange={changeSelectCategory}
         >
-          <option defaultValue={0}>Open this select menu</option>
-          <option value="1">Interrogation</option>
-          <option value="2">Pilot</option>
-          <option value="3">Scout</option>
-          <option value="4">Astromech</option>
-          <option value="5">Battle</option>
-          <option value="6">Assassin</option>
-          <option value="7">Protocol</option>
-          <option value="8">Torture</option>
+          <option selected disabled> -- Pick Category -- </option>
+          <option value="Interrogation">Interrogation</option>
+          <option value="Pilot">Pilot</option>
+          <option value="Scout">Scout</option>
+          <option value="Astromech">Astromech</option>
+          <option value="Battle">Battle</option>
+          <option value="Assassin">Assassin</option>
+          <option value="Protocol">Protocol</option>
+          <option value="Torture">Torture</option>
         </select>
 
         <label htmlFor="statusList" className="form-label mt-2">
           Select status
         </label>
         <select
-          className="form-select"
+          className="form-select mb-4"
           aria-label="Default select example"
           id="statusList"
+          onChange={changeSelectStatus}
         >
-          <option defaultValue={0}>Open this select menu</option>
+          <option selected disabled> -- Pick Status -- </option>
           <option value="New">New</option>
           <option value="Used">Used</option>
         </select>
-
-        <label htmlFor="priceRange" className="form-label mt-2">
-          Price range
-        </label>
-        <input
-          type="range"
-          className="form-range"
-          min="0"
-          max="5"
-          id="priceRange"
-        />
         <div className="d-grid gap-2 mt-2">
           <button type="button" className="btn btn-dark">
             Apply filter
